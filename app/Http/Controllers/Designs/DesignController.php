5f14af92233bd067c5de288eb803293aefefa42e
RampsosPeck <peralta.jorge.uatf@gmail.com>
2020-04-26 00:07:20 -0400

    Finish class ARREGLO class 143

--- app/Http/Controllers/Designs/DesignController.php
+++ app/Http/Controllers/Designs/DesignController.php
@@ -31,7 +31,7 @@ class DesignController extends Controller
 		$designs = $this->designs->withCriteria([
 				new LatestFirst(),
 				new IsLive(),
-				new ForUser(1),
+				new ForUser(2),
 				new EagerLoad(['user','comments'])
 			])->all();
 		return DesignResource::collection($designs);
